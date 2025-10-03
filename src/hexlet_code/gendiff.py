7540ac76fd05caedc16834d38229ee62e9b580de
grimmlok <Shinigami_626@mail.ru>
2025-10-03 13:56:33 +0300

    Настроены путевые точки входа, добавлена установка через uv, активирован виртуальный venv

---
+++ src/hexlet_code/gendiff.py
@@ -0,0 +1,15 @@
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Compares two configuration files and shows a difference.'
+    )
+    parser.add_argument('first_file', help='First configuration file')
+    parser.add_argument('second_file', help='Second configuration file')
+    args = parser.parse_args()
+
+    # Пока просто выводим имена файлов (в дальнейшем реализуйте сравнение)
+    print(f"Comparing files: {args.first_file} and {args.second_file}")
+
+if __name__ == "__main__":
+    main()
