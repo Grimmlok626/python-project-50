1042a2008c5c605804dca3eae3ff48b08142c3c4
grimmlok <Shinigami_626@mail.ru>
2025-10-03 14:30:13 +0300

    модифицировал скрипт gendiff

--- src/hexlet_code/gendiff.py
+++ src/hexlet_code/gendiff.py
@@ -4,12 +4,25 @@ def main():
     parser = argparse.ArgumentParser(
         description='Compares two configuration files and shows a difference.'
     )
-    parser.add_argument('first_file', help='First configuration file')
-    parser.add_argument('second_file', help='Second configuration file')
+    parser.add_argument('first_file', type=str, help='Path to the first file')
+    parser.add_argument('second_file', type=str, help='Path to the second file')
+    parser.add_argument(
+        '-f',
+        '--format',
+        dest='format',
+        type=str,
+        default='plain',
+        help='set format of output'
+    )
+
     args = parser.parse_args()
 
-    # Пока просто выводим имена файлов (в дальнейшем реализуйте сравнение)
-    print(f"Comparing files: {args.first_file} and {args.second_file}")
+    # Здесь вы можете добавить вызов функции сравнения файлов,
+    # передав ей args.first_file, args.second_file, args.format
 
-if __name__ == "__main__":
+    print(f'File 1: {args.first_file}')
+    print(f'File 2: {args.second_file}')
+    print(f'Selected format: {args.format}')
+
+if __name__ == '__main__':
     main()
