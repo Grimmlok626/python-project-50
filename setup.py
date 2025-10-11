1967ea27c5cccbea5aa7e367bc13aea0083b0a8b
grimmlok <Shinigami_626@mail.ru>
2025-10-11 06:57:18 +0300

    исправил указанные мне недостатки

--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="gendiff",
+    name="hexlet-code",
     version="0.1.0",
     description="Compares two configuration files and shows a difference.",
-    packages=find_packages(),
-    entry_points={
-        "console_scripts": [
-            "gendiff=gendiff.gendiff:main",
-        ],
-    },
-    install_requires=[
-        # Укажите зависимости, если есть
-    ],
     author="Ваше имя",
     author_email="your.email@example.com",
     url="https://github.com/Grimmlok626/python-project-50",
     python_requires=">=3.6",
+    packages=find_packages(where="src"),  # искать пакеты внутри src/
+    package_dir={"": "src"},              # src = корень пакетов
+    py_modules=["gendiff"],               # добавить одиночный модуль gendiff.py
+    entry_points={
+        "console_scripts": [
+            "gendiff=gendiff:main",       # точка входа для CLI
+        ],
+    },
+    install_requires=[],
 )
--- src/gendiff.py
+++ src/gendiff.py
@@ -1,1 +1,3 @@
-from hexlet_code.gendiff import *
+from hexlet_code.gendiff import generate_diff, main
+
+__all__ = ["generate_diff", "main"]
